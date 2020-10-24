1fd17877d1f5daaad52d77df2c0d355a63c6086e
surfing2003 <surfing2003@naver.com>
2020-10-24 23:02:28 +0900

    decorator 추가

---
+++ src/decorator/Main.java
@@ -0,0 +1,17 @@
+package decorator;
+
+public class Main {
+    public static void main(String[] args){
+
+        new XWingFighter().attack();
+
+        new LaserDecorator(new XWingFighter()).attack();
+
+        new PlasmaDecorator(
+                new MissileDecorator(
+                        new LaserDecorator(
+                                new XWingFighter()
+        ))).attack();
+
+    }
+}
